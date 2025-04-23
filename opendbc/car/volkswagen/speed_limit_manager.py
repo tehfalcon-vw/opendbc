6c463306bc516799d7c46ba088c5d2fa68c971de
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2025-04-23 23:11:06 +0200

    Create speed_limit_manager.py

---
+++ opendbc/car/volkswagen/speed_limit_manager.py
@@ -0,0 +1,40 @@
+from opendbc.car.common.conversions import Conversions as CV
+from opendbc.car.volkswagen.values import VolkswagenFlags
+
+class SpeedLimitManager:
+  def __init__(self, car_params):
+    self.CP = car_params
+    self.v_limit = 0.
+    self.v_limit_receive = False
+    self.v_limit_speed_factor = 0.
+
+  def update(self, cp):
+    if not self.CP.flags & VolkswagenFlags.MEB:
+      return
+
+    psd_06 = cp.vl["PSD_06"]
+    mux = psd_06["PSD_06_Mux"]
+
+    if mux == 2:
+      if (self.v_limit_receive and
+          psd_06["PSD_Ges_Typ"] == 1 and
+          psd_06["PSD_Ges_Gesetzlich_Kategorie"] == 0):
+
+        raw_speed = psd_06["PSD_Ges_Geschwindigkeit"]
+        if 0 < raw_speed < 11:
+          self.v_limit = (raw_speed - 1) * 5
+        elif 11 <= raw_speed < 23:
+          self.v_limit = 50 + (raw_speed - 11) * 10
+        else:
+          self.v_limit = 0
+
+        self.v_limit *= self.v_limit_speed_factor
+        self.v_limit_receive = False
+
+    elif mux == 0:
+      unit = psd_06["PSD_Sys_Geschwindigkeit_Einheit"]
+      self.v_limit_speed_factor = CV.MPH_TO_MS if unit == 1 else CV.KPH_TO_MS if unit == 0 else 0
+      self.v_limit_receive = psd_06["PSD_Sys_Quali_Tempolimits"] == 7
+
+  def get_speed_limit(self):
+    return self.v_limit
