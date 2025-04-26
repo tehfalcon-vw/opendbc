895b307f4f5b0d2aa426eac30c2fee0394799e59
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2025-04-26 14:05:11 +0200

    Update speed_limit_manager.py

--- opendbc/car/volkswagen/speed_limit_manager.py
+++ opendbc/car/volkswagen/speed_limit_manager.py
@@ -35,42 +35,52 @@ class SpeedLimitManager:
       
     return speed * self.v_limit_speed_factor
 
-  def receive_speed_limit(self, mux, psd_06):
+  def receive_speed_limit_fusion(self, mux, psd_06):
     if mux == 2:
       if (self.v_limit_receive and
           psd_06["PSD_Ges_Typ"] == 1 and
           psd_06["PSD_Ges_Gesetzlich_Kategorie"] == 0):
 
         raw_speed = psd_06["PSD_Ges_Geschwindigkeit"]
-        self.v_limit = convert_raw_speed(raw_speed)
+        self.v_limit = self.convert_raw_speed(raw_speed)
 
         self.v_limit_receive = False
 
-  def receive_street_type(self, mux, psd_06):
-    self.street_type == STREET_TYPE_URBAN
+  def receive_street_type(self, psd_04):
+    if psd_04["PSD_Segment_Komplett"] == 1 and psd_04["PSD_wahrscheinlichster_Pfad"] == 1:
+      if psd_04["PSD_Strassenkategorie"] == 1: # base type: urban
+        self.street_type = STREET_TYPE_URBAN
+          
+      elif psd_04["PSD_Strassenkategorie"] in (2, 3, 4): # base type: non urban
+        if psd_04["PSD_Bebauung"] == 1:
+          self.street_type = STREET_TYPE_URBAN
+        else:
+          self.street_type = STREET_TYPE_NONURBAN
+        
+      elif psd_04["PSD_Strassenkategorie"] == 5: # baste type: highway
+        self.street_type = STREET_TYPE_HIGHWAY
 
   def receive_speed_limit_legal(self, mux, psd_06):
     if mux == 2:
       if psd_06["PSD_Ges_Typ"] == 2:
-        if (psd_06["PSD_Ges_Gesetzlich_Kategorie"] == STREET_TYPE_URBAN    and self.street_type == STREET_TYPE_URBAN) or
-           (psd_06["PSD_Ges_Gesetzlich_Kategorie"] == STREET_TYPE_NONURBAN and self.street_type == STREET_TYPE_NONURBAN) or
-           (psd_06["PSD_Ges_Gesetzlich_Kategorie"] == STREET_TYPE_HIGHWAY  and self.street_type == STREET_TYPE_HIGHWAY):
+        if ((psd_06["PSD_Ges_Gesetzlich_Kategorie"] == STREET_TYPE_URBAN    and self.street_type == STREET_TYPE_URBAN) or
+            (psd_06["PSD_Ges_Gesetzlich_Kategorie"] == STREET_TYPE_NONURBAN and self.street_type == STREET_TYPE_NONURBAN) or
+            (psd_06["PSD_Ges_Gesetzlich_Kategorie"] == STREET_TYPE_HIGHWAY  and self.street_type == STREET_TYPE_HIGHWAY)):
           raw_speed = psd_06["PSD_Ges_Geschwindigkeit"]
-          self.v_limit = convert_raw_speed(raw_speed)
+          self.v_limit = self.convert_raw_speed(raw_speed)
 
-  def update(self, cp):
+  def update(self, psd_06, psd_04):
     if not self.CP.flags & VolkswagenFlags.MEB:
       return
 
-    psd_06 = cp.vl["PSD_06"]
     mux = psd_06["PSD_06_Mux"]
-
-    receive_street_type(mux, psd_06)
-    receive_speed_limit_permission(mux, psd_06)
-    receive_speed_factor(mux, psd_06)
-    receive_speed_limit_fusion(mux, psd_06) # try reading speed from car camera + navigation fusion
-    if self.v_limit == SPEED_LIMIT_NOT_SET:
-      receive_speed_limit_legal(mux, psd_06) # try reading speed from legal limit by current street type
+    
+    self.receive_speed_limit_permission(mux, psd_06)
+    self.receive_speed_factor(mux, psd_06)
+    self.receive_speed_limit_fusion(mux, psd_06) # try reading speed from car camera + navigation fusion
+    if psd_04 and self.v_limit == SPEED_LIMIT_NOT_SET:
+      self.receive_street_type(psd_04)
+      self.receive_speed_limit_legal(mux, psd_06) # try reading speed from legal limit by current street type
     
   def get_speed_limit(self):
     return self.v_limit
