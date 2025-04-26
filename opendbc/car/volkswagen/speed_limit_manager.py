37d17c5d8c3165c6f50a290c79e8a001a60e5f4a
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2025-04-26 12:40:54 +0200

    Update speed_limit_manager.py

--- opendbc/car/volkswagen/speed_limit_manager.py
+++ opendbc/car/volkswagen/speed_limit_manager.py
@@ -3,12 +3,22 @@ from opendbc.car.volkswagen.values import VolkswagenFlags
 
 SPEED_LIMIT_NOT_SET = 0
 
+
 class SpeedLimitManager:
   def __init__(self, car_params):
     self.CP = car_params
     self.v_limit = SPEED_LIMIT_NOT_SET
     self.v_limit_receive = False
     self.v_limit_speed_factor = CV.KPH_TO_MS
+
+  def receive_speed_factor(self, mux, psd_06):
+    if mux == 0:
+      unit = psd_06["PSD_Sys_Geschwindigkeit_Einheit"]
+      self.v_limit_speed_factor = CV.MPH_TO_MS if unit == 1 else CV.KPH_TO_MS if unit == 0 else 0
+
+  def receive_speed_limit_permission(self, mux, psd_06):
+    if mux == 0:
+      self.v_limit_receive = psd_06["PSD_Sys_Quali_Tempolimits"] == 7
 
   def receive_speed_limit(self, mux, psd_06):
     if mux == 2:
@@ -27,11 +37,6 @@ class SpeedLimitManager:
         self.v_limit *= self.v_limit_speed_factor
         self.v_limit_receive = False
 
-    elif mux == 0:
-      unit = psd_06["PSD_Sys_Geschwindigkeit_Einheit"]
-      self.v_limit_speed_factor = CV.MPH_TO_MS if unit == 1 else CV.KPH_TO_MS if unit == 0 else 0
-      self.v_limit_receive = psd_06["PSD_Sys_Quali_Tempolimits"] == 7
-
   def receive_speed_limit_legal(self, mux, psd_06):
     # TODO
     self.v_limit = self.v_limit
@@ -43,6 +48,8 @@ class SpeedLimitManager:
     psd_06 = cp.vl["PSD_06"]
     mux = psd_06["PSD_06_Mux"]
 
+    receive_speed_limit_permission(mux, psd_06)
+    receive_speed_factor(mux, psd_06)
     receive_speed_limit_fusion(mux, psd_06) # try reading speed from car camera + navigation fusion
     if self.v_limit == SPEED_LIMIT_NOT_SET:
       receive_speed_limit_legal(mux, psd_06) # try reading speed from legal limit by current street type
