29699eb7952f51e471541e405386094d1d79a478
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2025-04-27 10:48:00 +0200

    Update speed_limit_manager.py implement vze

--- opendbc/car/volkswagen/speed_limit_manager.py
+++ opendbc/car/volkswagen/speed_limit_manager.py
@@ -7,35 +7,58 @@ STREET_TYPE_NOT_SET = 0
 STREET_TYPE_URBAN = 1
 STREET_TYPE_NONURBAN = 2
 STREET_TYPE_HIGHWAY = 3
+SANITY_CHECK_DIFF_PERCENT_LOWER = 30
+SPEED_LIMIT_UNLIMITED_VZE_MS = 45
 
 
 class SpeedLimitManager:
   def __init__(self, car_params):
     self.CP = car_params
-    self.v_limit = SPEED_LIMIT_NOT_SET
-    self.v_limit_legal = SPEED_LIMIT_NOT_SET
+    self.v_limit_psd = SPEED_LIMIT_NOT_SET
+    self.v_limit_psd_legal = SPEED_LIMIT_NOT_SET
+    self.v_limit_vze = SPEED_LIMIT_NOT_SET
     self.v_limit_receive = False
     self.v_limit_speed_factor = CV.KPH_TO_MS
     self.street_type = STREET_TYPE_NOT_SET
+    self.v_limit_vze_sanity_error = False
+    self.v_limit_output_last = SPEED_LIMIT_NOT_SET
 
-  def update(self, psd_06, psd_04):
+  def update(self, psd_06, psd_04, vze):
     if not self.CP.flags & VolkswagenFlags.MEB:
       return
 
-    mux = psd_06["PSD_06_Mux"]
+    # try reading speed form trafic sign recognition
+    self._receive_speed_limit_vze(vze)
     
+    # try reading speed from predictive street data
+    mux = psd_06["PSD_06_Mux"]
     self._receive_speed_limit_permission(mux, psd_06)
     self._receive_speed_factor(mux, psd_06)
-    self._receive_speed_limit_fusion(mux, psd_06) # try reading speed from car camera + navigation fusion
+    self._receive_speed_limit_psd(mux, psd_06)
+    
+    # try reading speed from legal limit by current street type from psd
     if psd_04:
       self._receive_street_type(psd_04)
-      self._receive_speed_limit_legal(mux, psd_06) # try reading speed from legal limit by current street type
+      self._receive_speed_limit_psd_legal(mux, psd_06)
     
   def get_speed_limit(self):
-    if self.v_limit == SPEED_LIMIT_NOT_SET:
-      return self.v_limit_legal
+    if (self.v_limit_vze != SPEED_LIMIT_NOT_SET and self.v_limit_vze_sanity_error != True):
+      v_limit_output = self.v_limit_vze
+    elif self.v_limit_psd != SPEED_LIMIT_NOT_SET:
+      v_limit_output = self.v_limit_psd
     else:
-      return self.v_limit
+      v_limit_output = self.v_limit_psd_legal
+
+    self.v_limit_vze_sanity_error = False
+    self.v_limit_output_last = v_limit_output
+    
+    return v_limit_output
+
+  def _speed_limit_vze_sanitiy_check(self, speed_limit_vze_new):
+    diff_p = 100 * speed_limit_vze_new / self.v_limit_output_last
+    self.v_limit_vze_sanity_error = True if diff_p < SANITY_CHECK_DIFF_PERCENT_LOWER else False
+    if speed_limit_vze_new > SPEED_LIMIT_UNLIMITED_VZE_MS: # unlimited sign detected: use psd logic for setting maximum speed
+      self.v_limit_vze_sanity_error = True
 
   def _receive_speed_factor(self, mux, psd_06):
     if mux == 0:
@@ -46,7 +69,7 @@ class SpeedLimitManager:
     if mux == 0:
       self.v_limit_receive = psd_06["PSD_Sys_Quali_Tempolimits"] == 7
 
-  def convert_raw_speed(self, raw_speed):
+  def _convert_raw_speed_psd(self, raw_speed):
     if 0 < raw_speed < 11: # 0 - 45 kph
       speed = (raw_speed - 1) * 5
     elif 11 <= raw_speed < 23: # 50 - 160 kph
@@ -61,14 +84,24 @@ class SpeedLimitManager:
       
     return speed * self.v_limit_speed_factor
 
-  def _receive_speed_limit_fusion(self, mux, psd_06):
+  def _receive_speed_limit_vze(self, vze):
+    if vze["VZE_Verkehrszeichen_1_Typ"] == 0:
+      v_limit_vze = int(round(vze["VZE_Verkehrszeichen_1"]))# main traffic sign
+      if vze["VZE_Verkehrszeichen_Einheit"] == 1:
+        v_limit_vze *= CV.KPH_TO_MS
+      else:
+        v_limit_vze *= CV.MPH_TO_MS
+      self._speed_limit_vze_sanitiy_check(v_limit_vze)
+      self.v_limit_vze = v_limit_vze
+
+  def _receive_speed_limit_psd(self, mux, psd_06):
     if mux == 2:
       if (self.v_limit_receive and
           psd_06["PSD_Ges_Typ"] == 1 and
           psd_06["PSD_Ges_Gesetzlich_Kategorie"] == 0):
 
         raw_speed = psd_06["PSD_Ges_Geschwindigkeit"]
-        self.v_limit = self.convert_raw_speed(raw_speed)
+        self.v_limit_psd = self._convert_raw_speed_psd(raw_speed)
 
         self.v_limit_receive = False
 
@@ -86,11 +119,11 @@ class SpeedLimitManager:
       elif psd_04["PSD_Strassenkategorie"] == 5: # base type: highway
         self.street_type = STREET_TYPE_HIGHWAY
 
-  def _receive_speed_limit_legal(self, mux, psd_06):
+  def _receive_speed_limit_psd_legal(self, mux, psd_06):
     if mux == 2:
       if psd_06["PSD_Ges_Typ"] == 2:
         if ((psd_06["PSD_Ges_Gesetzlich_Kategorie"] == STREET_TYPE_URBAN    and self.street_type == STREET_TYPE_URBAN) or
             (psd_06["PSD_Ges_Gesetzlich_Kategorie"] == STREET_TYPE_NONURBAN and self.street_type == STREET_TYPE_NONURBAN) or
             (psd_06["PSD_Ges_Gesetzlich_Kategorie"] == STREET_TYPE_HIGHWAY  and self.street_type == STREET_TYPE_HIGHWAY)):
           raw_speed = psd_06["PSD_Ges_Geschwindigkeit"]
-          self.v_limit_legal = self.convert_raw_speed(raw_speed)
+          self.v_limit_psd_legal = self._convert_raw_speed_psd(raw_speed)
