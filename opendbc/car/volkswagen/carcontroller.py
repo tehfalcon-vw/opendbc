e53d89c8b6ba0f657f3f69af2520c6f15b454341
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2025-04-27 16:19:32 +0200

    Update carcontroller.py

--- opendbc/car/volkswagen/carcontroller.py
+++ opendbc/car/volkswagen/carcontroller.py
@@ -353,9 +353,12 @@ class CarController(CarControllerBase):
         self.gra_down = False
 
     # TESTING
+    if self.frame % 50 == 0:
+      self.gra_up = True
     if gra_send_ready:
       can_sends.append(self.CCS.create_gra_buttons_control_2(self.packer_pt, CANBUS.main, CS.gra_stock_values,
-                                                             up=True, down=self.gra_down))
+                                                             up=self.gra_up, down=self.gra_down))
+      self.gra_up = False
 
     new_actuators = actuators.as_builder()
     new_actuators.torque = self.apply_torque_last / self.CCP.STEER_MAX
