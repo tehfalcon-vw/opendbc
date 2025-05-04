c6dc8f545f82937837ce396ad2de2e3520e38d2b
infiniteCable2 <steffen.dahansi.werner@googlemail.com>
2025-05-04 21:35:17 +0200

    Update safety_volkswagen_meb.h

--- opendbc/safety/safety/safety_volkswagen_meb.h
+++ opendbc/safety/safety/safety_volkswagen_meb.h
@@ -154,7 +154,7 @@ static void volkswagen_meb_rx_hook(const CANPacket_t *to_push) {
       int current_curvature = ((GET_BYTE(to_push, 5U) & 0x7F) << 8 | GET_BYTE(to_push, 4U));
       
       bool current_curvature_sign = GET_BIT(to_push, 55U);
-      if (current_curvature_sign) {
+      if (!current_curvature_sign) {
         current_curvature *= -1;
       }
 
@@ -258,7 +258,7 @@ static bool volkswagen_meb_tx_hook(const CANPacket_t *to_send) {
     int desired_curvature_raw = (GET_BYTE(to_send, 3U) | (GET_BYTE(to_send, 4U) & 0x7FU << 8));
 
     bool desired_curvature_sign = GET_BIT(to_send, 39U);
-    if (desired_curvature_sign) {
+    if (!desired_curvature_sign) {
       desired_curvature_raw *= -1;
     }
 
