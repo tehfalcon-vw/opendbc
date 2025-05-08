850af3a309b5615c0575653bd3d862f02ebb06a7
infiniteCable2 <steffen.dahansi.werner@googlemail.com>
2025-05-08 12:12:53 +0200

    Update carcontroller.py

--- opendbc/car/volkswagen/carcontroller.py
+++ opendbc/car/volkswagen/carcontroller.py
@@ -213,10 +213,11 @@ class CarController(CarControllerBase):
     # **** Blinker Controls ************************************************** #
     # "Wechselblinken" has to be allowed in assistance blinker functions in gateway
     if self.CP.flags & VolkswagenFlags.MEB:
-      if self.frame % 40 == 0:
+      if self.frame % 400 == 0:
         self.blinker_takt = True if self.blinker_takt == False else True
       if self.frame % 2 == 0:
         can_sends.append(mebcan.create_blinker_control(self.packer_pt, CANBUS.pt, CS.ea_hud_stock_values, self.blinker_takt, left_blinker=True, right_blinker=CC.rightBlinker))
+        self.blinker_takt = True if self.blinker_takt == False else True
 
     # **** Cruise Controls ************************************************** #
     
