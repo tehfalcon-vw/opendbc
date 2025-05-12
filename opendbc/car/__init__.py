d683b2f16e91f38fd044463446f452e0f3d0ac96
infiniteCable2 <steffen.dahansi.werner@googlemail.com>
2025-05-12 16:03:12 +0200

    Update __init__.py

--- opendbc/car/__init__.py
+++ opendbc/car/__init__.py
@@ -199,9 +199,16 @@ def apply_std_curvature_limits(apply_curvature: float, apply_curvature_last: flo
                                  
   # Lateral acceleration
   # roll is passed to panda via custom Panda Data CAN message for internal usage only (not sent to car)
-  roll_compensation = roll * ACCELERATION_DUE_TO_GRAVITY 
-  max_lat_accel = ISO_LATERAL_ACCEL + roll_compensation
-  min_lat_accel = -ISO_LATERAL_ACCEL + roll_compensation
+  if apply_curvature_last > 0:  # right curve
+    max_lat_accel = ISO_LATERAL_ACCEL - (roll * ACCELERATION_DUE_TO_GRAVITY)
+    min_lat_accel = -ISO_LATERAL_ACCEL - (roll * ACCELERATION_DUE_TO_GRAVITY)
+  elif apply_curvature_last < 0:  # left curve
+    max_lat_accel = ISO_LATERAL_ACCEL + (roll * ACCELERATION_DUE_TO_GRAVITY)
+    min_lat_accel = -ISO_LATERAL_ACCEL + (roll * ACCELERATION_DUE_TO_GRAVITY)
+  else:
+    max_lat_accel = ISO_LATERAL_ACCEL
+    min_lat_accel = -ISO_LATERAL_ACCEL
+  
   max_curvature = max_lat_accel / (max(v_ego, 1.0) ** 2)
   min_curvature = min_lat_accel / (max(v_ego, 1.0) ** 2)
                                  
