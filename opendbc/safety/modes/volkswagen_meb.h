11ac055ead2a9bc497929727a31ae28965d9b4e4
infiniteCable2 <steffen.dahansi.werner@googlemail.com>
2025-05-28 16:20:29 +0200

    adapt to upstream

--- opendbc/safety/modes/volkswagen_meb.h
+++ opendbc/safety/modes/volkswagen_meb.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#include "safety_declarations.h"
-#include "safety_volkswagen_common.h"
+#include "opendbc/safety/safety_declarations.h"
+#include "opendbc/safety/modes/volkswagen_common.h"
 
 #define MSG_ESC_51           0xFC    // RX, for wheel speeds
 #define MSG_Motor_54         0x14C   // RX, for accel pedal
