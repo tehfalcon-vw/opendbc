c13e35ab5f12a012821c36962adcfbea63fb4b7c
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2025-06-14 20:43:20 +0200

    Update interface.py

--- opendbc/car/volkswagen/interface.py
+++ opendbc/car/volkswagen/interface.py
@@ -92,12 +92,9 @@ class CarInterface(CarInterfaceBase):
       ret.steerActuatorDelay = 0.4
       ret.lateralTuning.pid.kpBP = [15., 25., 35.]
       ret.lateralTuning.pid.kiBP = [15., 25., 35.]
-      ret.lateralTuning.pid.kf = 0.8
-      ret.lateralTuning.pid.kpV = [0.2, 0.75, 1.25]
-      ret.lateralTuning.pid.kiV = [0., 0.025, 0.08]
-      #ret.lateralTuning.pid.kf = 1.
-      #ret.lateralTuning.pid.kpV = [0., 0.6, 1.0]
-      #ret.lateralTuning.pid.kiV = [0., 0.02, 0.07]
+      ret.lateralTuning.pid.kf = 1.
+      ret.lateralTuning.pid.kpV = [0., 0.6, 1.1]
+      ret.lateralTuning.pid.kiV = [0., 0.02, 0.07]
     else:
       ret.steerActuatorDelay = 0.1
       ret.lateralTuning.pid.kpBP = [0.]
