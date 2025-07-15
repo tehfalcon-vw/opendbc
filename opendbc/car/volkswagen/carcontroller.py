45bc7b3bc75ac52923c73bedd4e496219d4f9d52
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2025-07-15 15:47:58 +0200

    Update carcontroller.py

--- opendbc/car/volkswagen/carcontroller.py
+++ opendbc/car/volkswagen/carcontroller.py
@@ -138,7 +138,7 @@ class CarController(CarControllerBase):
       # propably EA is stock activated only for cars equipped with capacitive steering wheel
       # (also stock long does resume from stop as long as hands on is detected additionally to OP resume spam)
       if self.frame % 6 == 0:
-        can_sends.append(mebcan.create_capacitive_wheel_touch(self.packer_pt, self.ext_bus, CC.enabled, CS.klr_stock_values))
+        can_sends.append(mebcan.create_capacitive_wheel_touch(self.packer_pt, self.ext_bus, CC.latActive, CS.klr_stock_values))
 
     # **** Blinker Controls ************************************************** #
     # "Wechselblinken" has to be allowed in assistance blinker functions in gateway
