6f6876b528e2b48091ecf36e81338beb0c4e8120
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2025-07-16 21:12:17 +0200

    Update interface.py

--- opendbc/car/volkswagen/interface.py
+++ opendbc/car/volkswagen/interface.py
@@ -121,12 +121,13 @@ class CarInterface(CarInterfaceBase):
 
     ret.pcmCruise = not ret.openpilotLongitudinalControl
     ret.vEgoStarting = 0.1
-    ret.vEgoStopping = 0.5
     ret.autoResumeSng = ret.minEnableSpeed == -1
 
     if ret.flags & VolkswagenFlags.MEB:
+      ret.vEgoStopping = 0.1
       ret.stopAccel = -1.1 # stock stopped accel
     else:
+      ret.vEgoStopping = 0.5
       ret.stopAccel = -0.55
 
     return ret
