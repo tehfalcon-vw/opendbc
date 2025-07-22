4aef8848ae543dd9e171e4d1c862993ae4b36819
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2025-07-22 18:31:35 +0200

    Update interface.py

--- opendbc/car/volkswagen/interface.py
+++ opendbc/car/volkswagen/interface.py
@@ -93,7 +93,7 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kpBP = [10., 40.]
       ret.lateralTuning.pid.kiBP = [10.] #, 40.]
       ret.lateralTuning.pid.kf = 1.
-      ret.lateralTuning.pid.kpV = [0., 1.45]
+      ret.lateralTuning.pid.kpV = [0., 1.] #1.45]
       ret.lateralTuning.pid.kiV = [0.] #, 0.12]
       ret.lateralTuning.pid.carCurvatureCorrection = True
     else:
