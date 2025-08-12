1ff4609b162370cadc48d6473904bff94bd6c490
infiniteCable2 <steffen.dahansi.werner@googlemail.com>
2025-08-12 17:51:24 +0200

    Update carstate.py revert starting state logic

--- opendbc/car/volkswagen/carstate.py
+++ opendbc/car/volkswagen/carstate.py
@@ -281,7 +281,6 @@ class CarState(CarStateBase):
     ret.brakePressed = bool(pt_cp.vl["Motor_14"]["MO_Fahrer_bremst"]) # includes regen braking by user
     ret.brake        = pt_cp.vl["ESC_51"]["Brake_Pressure"]
     ret.parkingBrake = pt_cp.vl["Gateway_73"]["EPB_Status"] in (1, 4) # EPB closing or closed
-    ret.brakeHoldConfirmation = bool(pt_cp.vl["VMM_02"]["ESP_Hold"])
 
     # Update gear and/or clutch position data.
     ret.gearShifter = self.parse_gear_shifter(self.CCP.shifter_values.get(pt_cp.vl["Getriebe_11"]["GE_Fahrstufe"], None))
